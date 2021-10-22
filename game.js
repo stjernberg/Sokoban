6410a193d1a7e50ca2cc4fa04356dd2cceb790af
Sara Stjernberg <sara_stjernberg@hotmail.com>
2021-10-22 16:11:16 +0200

    Added stop at wall and blocks

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
 const gameBoard = document.getElementById("boardContainer");
-const PlayerPosition = {
-  X: 0,
-  Y: 0,
+const playerPosition = {
+  x: 0,
+  y: 0,
 };
+const goals = 0;
 
 const createGameBoard = () => {
   for (let col = 0; col < tileMap01.height; col++) {
@@ -24,17 +25,83 @@ const createGameBoard = () => {
           break;
         case "P":
           oneTile.classList.add("entity-player");
-          PlayerPosition.X = row;
-          PlayerPosition.Y = col;
+          //set the player position
+          playerPosition.x = row;
+          playerPosition.y = col;
           break;
         case "G":
-          oneTile.classList.add(Tiles.Goal);
+          oneTile.classList.add("tile-goal");
           break;
         default:
           break;
       }
 
       gameBoard.appendChild(oneTile);
+      document.addEventListener("keydown", arrowKeys);
+    }
+  }
+};
+
+const arrowKeys = (e) => {
+  //console.log("event", e);
+  switch (e.keyCode) {
+    case 38:
+      // up arrow
+      console.log("up");
+      movePlayer(0, -1);
+      break;
+    case 40:
+      // down arrow
+      movePlayer(0, 1);
+      console.log("down");
+      break;
+    case 37:
+      //  left arrow
+      movePlayer(-1, 0);
+      console.log("left");
+      break;
+    case 39:
+      // right arrow
+      movePlayer(1, 0);
+      console.log("right");
+      break;
+  }
+};
+
+const movePlayer = (x, y) => {
+  let nextPosition = document.getElementById(
+    playerPosition.x + x + "," + (playerPosition.y + y)
+  );
+
+  let currentPosition = document.getElementById(
+    playerPosition.x + "," + playerPosition.y
+  );
+
+  let nextBlockPosition = document.getElementById(
+    playerPosition.x + x * 2 + "," + (playerPosition.y + y * 2)
+  );
+
+  if (nextPosition.classList.contains("entity-block")) {
+    if (
+      !(
+        nextBlockPosition.classList.contains("tile-wall") ||
+        nextBlockPosition.classList.contains("entity-block")
+      )
+    ) {
+      nextBlockPosition.classList.add("entity-block");
+      nextPosition.classList.remove("entity-block");
+      nextPosition.classList.add("entity-player");
+      currentPosition.classList.remove("entity-player");
+
+      playerPosition.x += x;
+      playerPosition.y += y;
+    }
+  } else {
+    if (!nextPosition.classList.contains("tile-wall")) {
+      nextPosition.classList.add("entity-player");
+      currentPosition.classList.remove("entity-player");
+      playerPosition.x += x;
+      playerPosition.y += y;
     }
   }
 };
